491e7d4ebf76b03e9bf23a33733b6fd8c777b99c
Brandon Wright <brandonfullstack@gmail.com>
2023-06-18 18:16:16 -0400

    Add h1 and p elements

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,14 @@
 window.addEventListener("load", function() {
   const removeH1 = document.querySelector("h1");
   removeH1.remove();
+
+  const h1Element = document.createElement("h1");
+  h1Element.append("Webpage Recreation Practice");
+  const topBody1 = document.querySelector("body");
+  topBody1.append(h1Element);
+
+  const pElement = document.createElement("p");
+  pElement.append("The HTML of this webpage was created with JavaScript.");
+  const topBody2 = document.querySelector("body");
+  topBody2.append(pElement);
 });
